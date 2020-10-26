4789afb82944ca7e29a85ef90dd141c43c65054d
noorulain718 <noorulain0107@gmail.com>
2020-10-26 12:31:45 +0500

    first commit

---
+++ src/javaapplication/forloop/JavaApplicationForLoop.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication.forloop;
+
+/**
+ *
+ * @author aiman
+ */
+public class JavaApplicationForLoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i=0;i<=10;i=i+2)
+            System.out.println(i);
+    }
+    
+}
